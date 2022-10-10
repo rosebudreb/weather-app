984735c7543a35222ac23a53b434fb241e5dfa7c
Raechel <rosebudreb@yahoo.com>
2022-10-10 11:17:16 -0500

    Added 6 day forecast

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function setDate(timestamp) {
   let now = new Date(timestamp);
+  let currentDate = document.querySelector("#current-date");
+  let currentDay = document.querySelector("#current-day");
+  let currentTime = document.querySelector("#current-time");
+  currentDate.innerHTML = `${
+    now.getMonth() + 1
+  }/${now.getDate()}/${now.getFullYear()}`;
+  currentDay.innerHTML = getDayOfWeek(timestamp);
+  currentTime.innerHTML = `${now.getHours()}:${String(
+    now.getMinutes()
+  ).padStart(2, "0")}`;
+}
+
+function getDayOfWeek(timestamp) {
+  let now = new Date(timestamp);
+  console.log(timestamp);
+  console.log(now.getDay());
+  console.log(`${now.getHours()}:${String(now.getMinutes()).padStart(2, "0")}`);
   let days = [
     "Sunday",
     "Monday",
@@ -9,16 +26,7 @@ function setDate(timestamp) {
     "Friday",
     "Saturday",
   ];
-  let currentDate = document.querySelector("#current-date");
-  let currentDay = document.querySelector("#current-day");
-  let currentTime = document.querySelector("#current-time");
-  currentDate.innerHTML = `${
-    now.getMonth() + 1
-  }/${now.getDate()}/${now.getFullYear()}`;
-  currentDay.innerHTML = days[now.getDay()];
-  currentTime.innerHTML = `${now.getHours()}:${String(
-    now.getMinutes()
-  ).padStart(2, "0")}`;
+  return days[now.getDay()];
 }
 //Create Search Function
 function searchByForm(event) {
@@ -42,16 +50,51 @@ function setDegreesHTML(degrees, linkToDisable, linkToEnable) {
 }
 //Create html update function
 function setTempuratureHTML(response) {
+  // get lat and lon from response
+  let apiKey = "17ad6e67aa629189f73b053634668b20";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?cnt=6&lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&cnt=5&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(setForecastHTML);
+  // make new API call
+  // Call setForecastHTML with API result
+
   setDegreesHTML(response.data.main.temp, farenheitLink, celsiusLink);
   let weatherElement = document.querySelector("#weather-state");
   weatherElement.innerHTML = response.data.weather[0].description;
   let iconElement = document.querySelector("#icon");
   let city = document.querySelector("#current-location");
   city.innerHTML = response.data.name;
+
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+}
+function setForecastHTML(response) {
+  let forecastWrapper = document.querySelector("#forecast-wrapper");
+  let forecastHTML = "";
+  for (i = 0; i < 6; i++) {
+    let day = response.data.daily[i];
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+          <div class="card">
+            <div class="card-body">
+              ${getDayOfWeek(new Date(day.dt * 1000))}
+              <div class="emoji-weather">
+                <img class="" src="http://openweathermap.org/img/wn/${
+                  day.weather[0].icon
+                }@2x.png" />
+              </div>
+              <div class="degrees-day"><span class="high-temp-forecast">${Math.round(
+                day.temp.max
+              )}</span>°/<span class="low-temp-forecast">${Math.round(
+        day.temp.min
+      )}</span>°</div>
+            </div>
+          </div>
+        </div>`;
+  }
+  forecastWrapper.innerHTML = forecastHTML;
 }
 //Create conversion function
 function displayCelsiusTemp(event) {
